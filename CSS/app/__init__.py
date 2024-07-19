d84633e5cdc0ee689bd8270c380649a483431083
ChaithraUmesh21 <138147671+ChaithraUmesh21@users.noreply.github.com>
2024-07-19 22:56:50 +0100

    bugfix

--- CSS/app/__init__.py
+++ CSS/app/__init__.py
@@ -3,13 +3,16 @@ from flask_socketio import SocketIO
 from flask_mail import Mail
 from pymongo import MongoClient
 from dotenv import load_dotenv
-
 import os
 from werkzeug.security import generate_password_hash
 
 load_dotenv()
 
-create_app():
+socketio = SocketIO()
+mail = Mail()
+mongo = None
+
+def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
     app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
@@ -17,11 +20,22 @@ create_app():
     app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
     app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS').lower() in ['true', '1', 't']
     app.config['MAIL_USE_SSL'] = os.getenv('MAIL_USE_SSL').lower() in ['true', '1', 't']
+    app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT'))
+
+    global mongo
+    mongo_uri = os.getenv('MONGO_URI')
+    mongo = MongoClient(mongo_uri).get_database()
+
+    socketio.init_app(app)
+    mail.init_app(app)
 
 from .routes import main as main_blueprint
-    app.register_blueprint(main_blueprint)
+app.register_blueprint(main_blueprint)
+
+from .sockets import init_sockets
+init_sockets(socketio)
 
 with app.app_context():
-        create_default_admin()
+    create_default_admin()
 
     return app
