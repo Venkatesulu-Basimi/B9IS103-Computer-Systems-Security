8a880e42be3539156eefc80319bc9ed8272ec10f
ChaithraUmesh21 <138147671+ChaithraUmesh21@users.noreply.github.com>
2024-07-20 22:09:11 +0100

    Bugfix

--- CSS/app/encryption.py
+++ CSS/app/encryption.py
@@ -3,7 +3,6 @@ from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import hashes
-
 import os
 from base64 import urlsafe_b64encode, urlsafe_b64decode
 
@@ -25,11 +24,10 @@ def generate_keys():
 def encrypt_message(public_key_pem, message):
     public_key = serialization.load_pem_public_key(public_key_pem.encode('utf-8'))
     session_key = os.urandom(32)
-    nonce = os.urandom(12)
-    cipher = Cipher(algorithms.AES(session_key), modes.GCM(nonce))
+    cipher = Cipher(algorithms.AES(session_key), modes.GCM(os.urandom(12)))
     encryptor = cipher.encryptor()
     encrypted_message = encryptor.update(message.encode('utf-8')) + encryptor.finalize()
-   
+
     encrypted_session_key = public_key.encrypt(
         session_key,
         padding.OAEP(
@@ -38,14 +36,9 @@ def encrypt_message(public_key_pem, message):
             label=None
         )
     )
-
-    # Return the encrypted session key, encrypted message, and the tag, all base64 encoded
-    return (
-        urlsafe_b64encode(encrypted_session_key).decode('utf-8'),
-        urlsafe_b64encode(encrypted_message).decode('utf-8'),
-        urlsafe_b64encode(encryptor.tag).decode('utf-8'),
-        urlsafe_b64encode(nonce).decode('utf-8')
-    )
+    return (urlsafe_b64encode(encrypted_session_key).decode('utf-8'),
+            urlsafe_b64encode(encrypted_message).decode('utf-8'),
+            urlsafe_b64encode(encryptor.tag).decode('utf-8'))
 
 def decrypt_message(private_key_pem, encrypted_session_key, encrypted_message, tag):
     private_key = serialization.load_pem_private_key(
